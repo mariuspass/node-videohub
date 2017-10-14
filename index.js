c19107470dc2dce1aa264a58f734e7b742c55dcf
Marius Dobre <marius@dobre.one>
2017-10-14 17:04:17 +0300

    Revert "initial promise based version"
    This reverts commit 4e61d47e579ef5c98115f3b3fc051bf188478b79.

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 /*!
- * node-videohub
+ * videohub
  *
- * Copyright (C) 2017  Sylvain Vriens
- *
+ * Copyright (C) 2014  Sylvain Vriens
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- module.exports = {
-   Device: require('./lib/Device')
- }
+module.exports = require('./lib/videohub');
+module.exports.aggregate = require('./lib/aggregate');
